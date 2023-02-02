b3ac07b4f7a0b91785a64b89696eef3eae7aa9e3
Obelem <ibbraidejesse@gmail.com>
2023-02-02 13:59:17 +0100

    debugging

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,7 @@ from typing import Mapping, Any, Union
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[~T, None] = None) -> Union[Any, ~T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """duck typed annotations"""
     if key in dct:
         return dct[key]
