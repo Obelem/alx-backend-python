ad781ad1fd1ac8bfcc9db377d450fc2f97f0a28b
Obelem <ibbraidejesse@gmail.com>
2023-02-02 13:58:45 +0100

    debugging

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""More involved type annotations"""
+from typing import Mapping, Any, Union
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[~T, None] = None) -> Union[Any, ~T]:
+    """duck typed annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
