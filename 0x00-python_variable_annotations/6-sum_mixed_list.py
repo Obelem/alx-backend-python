2039c88141018bae3c12dfbbfee2548bd6da08a2
Obelem <ibbraidejesse@gmail.com>
2023-02-02 12:47:52 +0100

    edited Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mixd_list: Union[int | float]) -> float:
+def sum_mixed_list(mixd_list: List[int, float]) -> float:
     """takes a list mxd_lst of integers and floats
     and returns their sum as a float.
 
