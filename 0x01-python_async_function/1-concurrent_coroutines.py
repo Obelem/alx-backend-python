1295bcb0d426b28aa4e7c14195e48dd97c2aabfb
Obelem <ibbraidejesse@gmail.com>
2023-02-06 11:21:02 +0100

    added return annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """execute multiple coroutines at the same time with async"""
 import random
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """execute concurrent coroutines
 
     Args:
