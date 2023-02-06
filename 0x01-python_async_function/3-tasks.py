fb91390976a69a5002394a24985167025d81a453
Obelem <ibbraidejesse@gmail.com>
2023-02-06 13:32:28 +0100

    documented function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 '''more async'''
-import random
 from asyncio import create_task, Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: float = 10) -> Task:
-    task = create_task(wait_random(max_delay))
-    return task
+    """concurrency without async
+
+    Args:
+        max_delay (float, optional): _description_. Defaults to 10.
+
+    Returns:
+        Task: _description_
+    """
+    return create_task(wait_random(max_delay))
