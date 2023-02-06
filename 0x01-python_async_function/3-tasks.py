1db8be840271ecec2e7850bdfb0af85b8d60ea49
Obelem <ibbraidejesse@gmail.com>
2023-02-06 13:03:24 +0100

    added return statement

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,4 +6,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: float = 10) -> Task:
-    return create_task(wait_random(max_delay))
+    task = create_task(wait_random(max_delay))
+    return task
