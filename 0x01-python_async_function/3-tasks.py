eaddf04da4b50ed542a9ffe71b451f1d9b7c24e8
Obelem <ibbraidejesse@gmail.com>
2023-02-06 13:01:50 +0100

    implemented concurrent code with create_task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''more async'''
+import random
+from asyncio import create_task, Task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: float = 10) -> Task:
+    return create_task(wait_random(max_delay))
