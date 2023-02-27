bc21e22ef61b5109f243b199ca8b0e6f75a941c8
Obelem <ibbraidejesse@gmail.com>
2023-02-27 20:42:28 +0100

    added pydocs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,21 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected_output):
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test that the access_nested_map function raises
+        a KeyError for invalid paths.
+
+        Args:
+            nested_map (dict): The nested dictionary to access.
+            path (tuple): The path of keys to access the nested dictionary.
+            expected (str): The expected exception message.
+
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected)
